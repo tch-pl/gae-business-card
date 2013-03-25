67dbb7d716b735a2f6cdfd8c930e4495cb0d999b
unknown <user@compton.(none)>
2013-03-25 23:06:49 +0100

    added new data

--- tch/cv/webapp/pages.py
+++ tch/cv/webapp/pages.py
@@ -36,10 +36,11 @@ class MenuItem():
         return current_description
         
 
-menu = [MenuItem('employment', "employment.html", {"pl":"Zatrudnienie", "en":"Employment"}),
+menu = [MenuItem('about', "about.html", {"pl":"O mnie", "en":"About myself"}),
+        MenuItem('employment', "employment.html", {"pl":"Zatrudnienie", "en":"Employment"}),
              MenuItem('education', "education.html", {"pl":"Edukacja", "en":"Education"}),
-             MenuItem('experience', "index.html", {"pl":"Doswiadczenie", "en":"Experience"}),
-             MenuItem('about', "about.html", {"pl":"O mnie", "en":"About myself"})]
+             MenuItem('experience', "index.html", {"pl":u"Doświadczenie", "en":"Experience"})
+             ]
 
 
 jinja_environment = jinja2.Environment(
@@ -109,7 +110,7 @@ class BusinessCard(webapp.RequestHandler):
 
     def resolveModel(self, language, controller_name):
         content = self.resolveContent(controller_name)
-        title = {'pl': 'Troche informacji o mnie', 'en': 'Some info about me'}
+        title = {'pl': u'Trochę informacji o mnie', 'en': 'Some info about me'}
         since_date = {'pl': 'Od', 'en': 'Since'}
         to_date = {'pl': 'Do', 'en': 'To'}
         model = {
@@ -152,45 +153,52 @@ class HistoryContent():
     def __init__(self, user=""):
         self.employmentHistory = [Employment("BLStream Sp. z o. o.",
                               '20011-06', '-',
-                              {'pl':'Techniczny Lider Projektu', 'en':'Technical Project Leader'},
-                              {'pl': ['Koordynacja pracy zespolu projektowego', 'Komunikacja z klientem', 'Wsparcie techniczne zespolu i klienta'],
-                                'en':['development team coordination', 'communication with customer', 'technical support for team and customer']}),
+                              {'pl':u'Techniczny Lider Projektu', 'en':'Technical Project Leader'},
+                              {'pl': [u'Koordynacja pracy zespołu projektowego', u'Komunikacja z klientem', u'Wsparcie techniczne zespołu i klienta'],
+                                'en':['development team coordination', 'Continuous communication and cooperation with customer', 'Technical support for team and customer']}),
                         Employment("BLStream Sp. z o. o.",
                               '20010-07', '2011-05',
-                              {'pl':'Starszy Programista',
+                              {'pl':u'Starszy Programista',
                                'en':'Senior Software Developer'},
-                              {'pl':['Utrzymanie i rozwoj aplikacji typu selfcare', 'Tworzenie dokumentacji technicznej'],
-                               'en':['Maintenance and development of selfcare web application', 'Writing technical documentation' ]}),
+                              {'pl':[u'Utrzymanie i rozwój aplikacji typu selfcare', u'Tworzenie dokumentacji technicznej', u'Analiza techniczna i biznesowa wymagań', u'Projektowanie rozwiązań (m.in. zmiany architektury, optymalizacje)', u'Rozwiązanie problemów produkcyjnych'],
+                               'en':['Maintenance and development of selfcare web application', 'Writing technical documentation', 'Technical and business analysis','Fixing and resolving problems on production', 'Solution design (including architecture changes, optimizations)' ]}),
                         Employment("BLStream Sp. z o. o.",
                               '2007-12', '2010-06',
                               {'pl':'Programista', 'en':'Software Developer'},
-                              {'pl':'', 'en':''}),
+                              {'pl':[u'Implementacja aplikacji webowych', u'Dbanie o jakość kodu, spójność wersji', u'Analiza techniczna wymagań', u'Tworzenie dokumentacji technicznej projektu'], 
+                               'en':['Web application implementation','Code quality assurance, application versioning','Technical analysis', 'Technical documentation writing']}),
                         Employment("Optix Sp. z o. o.",
                                    '2006-09', '2007-11',
-                                   {'pl':'Programista', 'en':'Software Developer'},
-                                   {'pl':'', 'en':''})]
+                                   {'pl':u'Programista systemów informatycznych', 'en':'Software Developer'},
+                                   {'pl':[u'Adaptacja systemów zarządzania obiegiem dokumentów do potrzeb klienta', u'Instalacja i konfiguracja oprogramowania specjalistycznego' ,u'Tworzenie dokumentacji technicznej projektu', u'Komunikacja z klientem biznesowym'], 'en':['The implementation of solutions based on the specification- adaptation of the workflow management systems to the customer needs (extensions implementation)' ,'Installation and configuration of specialized software', 'Technical documentation writing', 'Communication and cooperation with customer ']}),
+                        Employment(u'Urząd Miejski w Koszalinie',
+                                   '2005-12', '2006-06',
+                                   {'pl':'Specjalista IT', 'en':'IT Specialist'},
+                                   {'pl':[u'Udział w procesie tworzenia portalu www.biznes.koszalin.pl – analiza, testy użyteczności'], 'en':[u'Participation in the process of creating the portal www.biznes.koszalin.pl - analysis, usability testing']})]
         
-        self.educationHistory = [Education({"en":"Koszalin University of Technology", "pl":"Politechnika Koszalinska"},
-                              {'en':'October 2004', 'pl':'Pazdziernik 2004'},
-                              {'en':'February 2006','pl':'Luty 2006'},
+        self.educationHistory = [Education({"en":"Koszalin University of Technology", "pl":u"Politechnika Koszalińska"},
+                              {'en':'October 2004', 'pl':u'Październik 2004'},
+                              {'en':'February 2006','pl':u'Luty 2006'},
                               {'pl':'Magister', 'en':'Master\'s degree'},
-                              {'pl': ['Wydzial Elektroniki i informatyki', 
+                              {'pl': [u'Wydział Elektroniki i Informatyki', 
                                       'Specjalizacja - Systemy zarzadzania bazami danych', 
-                                      'Temat pracy magisterskiej - \"Pomiar uzytecznosci witryn i portali internetowych metoda programowa\"'],
+                                      u'Temat pracy magisterskiej - \"Pomiar użyteczności witryn i portali internetowych metodą programową\"'],
                                'en': ['Department of Electronics and Computer Science', 
                                       'Specialization - Database Management Systems', 
-                                      'Thesis - \"Web pages measuring with software\"']}),
+                                      'Thesis - \"Web pages measuring with software\"']},
+                                {'en':['Lecture at the conference SECON 2004 (Military University of Technology in Warsaw) entitled \"Evaluation of the usability of websites\"'], 'pl':[u'Wykład na konferencji SECON 2004 (Wojskowa Akademia Techniczna w Warszawie) pod tytułem \"Ocena użyteczności stron internetowych\"']}),
 
-                              Education({"en":"Koszalin University of Technology", "pl":"Politechnika Koszalinska"},
-                              {'en':'October 2000', 'pl':'Pazdziernik 2000'},
-                              {'en':'September 2004','pl':'Wrzesien 2004'},
-                              {'pl':'Inzynier', 'en':'Engineer\'s degree'},
-                              {'pl': ['Wydzial Elektroniki i informatyki', 
+                              Education({"en":"Koszalin University of Technology", "pl":u"Politechnika Koszalińska"},
+                              {'en':'October 2000', 'pl':u'Październik 2000'},
+                              {'en':'September 2004','pl':u'Wrzesień 2004'},
+                              {'pl':u'Inżynier', 'en':'Engineer\'s degree'},
+                              {'pl': ['Wydzial Elektroniki i Informatyki', 
                                       'Specjalizacja - Programowanie i sieci informatyczne', 
-                                      'Temat pracy inzynierskiej - \"Aplikacja Java do testowania uzytecznosci stron internetowych\"'],
+                                      u'Temat pracy inżynierskiej - \"Aplikacja Java do testowania użyteczności stron internetowych\"'],
                                'en': ['Department of Electronics and Computer Science', 
                                       'Specialization - Programming and Information Networks', 
-                                      'Thesis - \"Web page usalbility analyzer - Java application\"']})]
+                                      'Thesis - \"Web page usalbility analyzer - Java application\"']},
+                                {'en':['Publication entitled: \"Technological approach to web usability evaluation\"'], 'pl':[u'Publikacja pt. \"Technologiczne podejście do oceny użyteczności stron internetowych\"']})]
 
 class History():
     def __init__(self, start_date, end_date, description):
@@ -208,12 +216,13 @@ class Employment(History):
         self.description = description
 
 class Education(History):
-    def __init__(self, school, start_date, end_date, degree, description):
+    def __init__(self, school, start_date, end_date, degree, description, additional_data):
         self.school = school
         self.start_date = start_date
         self.end_date = end_date        
         self.description = description
         self.degree = degree
+        self.additional_data = additional_data
 
     
 application = webapp.WSGIApplication([('/', Main), (r'/cv.*', BusinessCard)], debug=True)
